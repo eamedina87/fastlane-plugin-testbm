76ce1e3f526eb2d1cb7e8124e658e5521aad2951
Erick Medina <erickmedinamoreno@hotmail.com>
2021-01-15 13:22:16 +0100

    print param

---
+++ lib/fastlane/plugin/testbm/actions/salutation_action.rb
@@ -0,0 +1,46 @@
+require 'fastlane/action'
+
+module Fastlane
+  module Actions
+    class SalutationAction < Action
+      def self.run(params)
+        UI.message("Hello World!" + params[:person_name])
+      end
+
+      def self.description
+        "Returns hello world"
+      end
+
+      def self.authors
+        ["Bemobile"]
+      end
+
+      def self.return_value
+        # If your method provides a return value, you can describe here what it does
+      end
+
+      def self.details
+        # Optional:
+        "Just a test plugin"
+      end
+
+      def self.available_options
+        [
+          # FastlaneCore::ConfigItem.new(key: :your_option,
+          #                         env_name: "TESTBM_YOUR_OPTION",
+          #                      description: "A description of your option",
+          #                         optional: false,
+          #                             type: String)
+        ]
+      end
+
+      def self.is_supported?(platform)
+        # Adjust this if your plugin only works for a particular platform (iOS vs. Android, for example)
+        # See: https://docs.fastlane.tools/advanced/#control-configuration-by-lane-and-by-platform
+        #
+        # [:ios, :mac, :android].include?(platform)
+        true
+      end
+    end
+  end
+end
